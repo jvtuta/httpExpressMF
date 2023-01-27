f45f491e0ed7553c3a341d46893c85be7ee6e6d4
unknown <joaovoliveira2399@gmail.com>
2023-01-27 17:45:51 -0300

    add method Provider for database

---
+++ src/database/Provider.js
@@ -0,0 +1,30 @@
+export default function (conn) {
+  const query = async (sql, params) => {
+    const [rows] = await conn.execute(sql, params);
+    return rows;
+  };
+
+  const insert = async (table, data) => {
+    const fields = Object.keys(data);
+    const values = Object.values(data);
+    const sql = `INSERT INTO ${table} (${fields.join(',')}) VALUES (${values.map(() => '?').join(',')})`;
+    const [result] = await conn.execute(sql, values);
+    return result;
+  }
+
+  const update = async (table, data, where) => {
+    const fields = Object.keys(data);
+    const values = Object.values(data);
+    const sql = `UPDATE ${table} SET ${fields.map(field => `${field} = ?`)} WHERE ${where}`;
+    const [result] = await conn.execute(sql, values);
+    return result;
+  }
+
+  const remove = async (table, where) => {
+    const sql = `DELETE FROM ${table} WHERE ${where}`;
+    const [result] = await conn.execute(sql);
+    return result;
+  }
+
+  return [ query, insert, update, remove ]
+}
